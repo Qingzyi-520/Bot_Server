8074f2f4eae81c619b671287491e137cf19b00dc
QingZyi <dionwijaya878@gmail.com>
2025-08-12 13:03:40 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+// index.js - Main entry point
+import {
+  Client,
+  GatewayIntentBits,
+  Partials
+} from 'discord.js';
+import 'dotenv/config';
+import { connectDB } from './database/connection.js';
+import { setupEventHandlers } from './events/index.js';
+import { CONFIG } from './config/config.js';
+
+// Create Discord client
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
+
+// Connect to database
+connectDB();
+
+// Setup all event handlers
+setupEventHandlers(client);
+
+// Login to Discord
+client.login(process.env.TOKEN);
+
+export { client };
